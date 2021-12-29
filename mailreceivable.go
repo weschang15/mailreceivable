c20a1cf8db644f97ecc84d20f080e19e6cd7ec51
Wesley Chang <weschang15@gmail.com>
2021-12-29 11:57:21 -0800

    feat: add funcs to validate email format and verify email host existence

---
+++ mailreceivable.go
@@ -0,0 +1,86 @@
+package mailreceivable
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"net/smtp"
+	"regexp"
+	"strings"
+	"time"
+)
+
+var (
+	ErrHostNotFound = errors.New("host not found")
+	ErrInvalidEmail = errors.New("invalid email format")
+	emailRegexp = regexp.MustCompile(`(?m)^(((((((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)|(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?"((\s? +)?(([!#-[\]-~])|(\\([ -~]|\s))))*(\s? +)?"))?)?(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?<(((((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?(([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+(\.([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+)*)((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)|(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?"((\s? +)?(([!#-[\]-~])|(\\([ -~]|\s))))*(\s? +)?"))@((((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?(([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+(\.([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+)*)((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)|(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?\[((\s? +)?([!-Z^-~]))*(\s? +)?\]((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)))>((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?))|(((((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?(([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+(\.([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+)*)((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)|(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?"((\s? +)?(([!#-[\]-~])|(\\([ -~]|\s))))*(\s? +)?"))@((((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?(([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+(\.([A-Za-z0-9!#-'*+\/=?^_\x60{|}~-])+)*)((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?)|(((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?\[((\s? +)?([!-Z^-~]))*(\s? +)?\]((((\s? +)?(\(((\s? +)?(([!-'*-[\]-~]*)|(\\([ -~]|\s))))*(\s? +)?\)))(\s? +)?)|(\s? +))?))))$`)
+)
+
+const timeoutDefault = time.Second * 10
+
+// Validate attempts to check the syntax of a provided email address.
+// It returns an error if the addrss is formatted incorrectly.
+func Validate(e string) error {
+	if !emailRegexp.MatchString(strings.ToLower(e)) {
+		return ErrInvalidEmail
+	}
+
+	return nil
+}
+
+// VerifyMX checks if the MX DNS records exist for the given domain
+func VerifyMX(d string) error {
+	if _, err := net.LookupMX(d); err != nil {
+		return ErrHostNotFound
+	}
+
+	return nil
+}
+
+// VerifyHost attempts to connect to the host associated with a provided email address.
+// It returns an error if the connection fails.
+func VerifyHost(e string) error {
+	_, host := split(e)
+	mx, err := net.LookupMX(host)
+
+	if err != nil {
+		return ErrHostNotFound
+	}
+
+	address := fmt.Sprintf("%s:%d", mx[0].Host, 25)
+	client, err := DialClient(address, timeoutDefault)
+
+	if err != nil {
+		return err
+	}
+
+	client.Close()
+	return nil
+}
+
+func DialClient(a string, t time.Duration) (*smtp.Client, error)  {
+	conn, err := net.DialTimeout("tcp", a, t);
+	if err != nil {
+		return nil, err
+	}
+
+	timer := time.AfterFunc(t, func() { conn.Close() })
+	defer timer.Stop()
+
+	host, _, _ := net.SplitHostPort(a)
+	return smtp.NewClient(conn, host)
+}
+
+// Split extracts the email user account and email host from a provided address.
+// It returns both the user and host
+func split(e string) (user, host string) {
+	index := strings.LastIndexByte(e, '@')
+	if index < 0 {
+		return 
+	}
+
+	user = e[:index]
+	host = e[index+1:]
+
+	return 
+}
